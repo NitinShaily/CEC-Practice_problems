c5c147d6cdb68d566792aabea173c0d7d85408e8
Nitin Shaily <nitinshaily0@gmail.com>
2021-02-24 23:11:28 +0530

    q9 added

--- pattern/q9.cpp
+++ pattern/q9.cpp
@@ -7,23 +7,22 @@ int main(){
     cin>>n;
 
     for(i=0; i<n; i++){
-        for(int k=0; k<i; k++)
+        for(int k=0; k<n-i-1; k++)
             cout<<" ";
-        for(j=n-i; j>0; j--){
-            cout<<j;
-        }
+        for(j=0; j<=i; j++)
+            cout<<"* ";
         cout<<"\n";
     }
+    return 0;
 }
 /*
-
-PS C:\Users\acer\CEC-Practice_problems\pattern> g++ q8.cpp
+PS C:\Users\acer\CEC-Practice_problems\pattern> g++ q9.cpp
 PS C:\Users\acer\CEC-Practice_problems\pattern> ./a.exe   
 5
-54321
- 4321
-  321
-   21
-    1
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
 
 */
