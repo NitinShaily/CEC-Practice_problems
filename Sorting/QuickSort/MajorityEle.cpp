71e58255deba4eacd8d89c5811360e770bfdf74d
Nitin Shaily <nitinshaily0@gmail.com>
2021-03-04 10:17:18 +0530

    updates

--- Sorting/QuickSort/MajorityEle.cpp
+++ Sorting/QuickSort/MajorityEle.cpp
@@ -2,14 +2,14 @@
 #include<algorithm>
 using namespace std;
 
-int findMajority(int *arr, int n){
+pair<bool, int> findMajority(int *arr, int n){
     sort(arr, arr+n);
     int half = n/2;
     for(int i=0; i<half-1; i++){
         if(arr[i]==arr[i+half])
-            return arr[i];
+            return make_pair(true, arr[i]);
     }
-    return -1;
+    return make_pair(false,-1);
 }
 int main(){
     int n;
@@ -18,9 +18,10 @@ int main(){
     for(int i=0; i<n; i++)
         cin>>arr[i];
 
-    int maj = findMajority(arr, n);
-    if(maj != -1)
-        cout<<"majority element: "<<maj;
+    pair<bool, int> maj;
+    maj = findMajority(arr, n);
+    if(maj.first)
+        cout<<"majority element: "<<maj.second;
     else
         cout<<"Element don't exist";
 
--- Sorting/QuickSort/MajorityOptimised.cpp
+++ Sorting/QuickSort/MajorityOptimised.cpp
@@ -2,14 +2,14 @@
 #include<algorithm>
 using namespace std;
 
-int findMajority(int *arr, int n){
+pair<bool, int> findMajority(int *arr, int n){
     sort(arr, arr+n);
     int half = n/2;
     for(int i=0; i<half-1; i++){
         if(arr[i]==arr[i+half])
-            return arr[i];
+            return make_pair(true, arr[i]);
     }
-    return -1;
+    return make_pair(false,-1);
 }
 int main(){
     int n;
@@ -18,9 +18,10 @@ int main(){
     for(int i=0; i<n; i++)
         cin>>arr[i];
 
-    int maj = findMajority(arr, n);
-    if(maj != -1)
-        cout<<"majority element: "<<maj;
+    pair<bool, int> maj;
+    maj = findMajority(arr, n);
+    if(maj.first)
+        cout<<"majority element: "<<maj.second;
     else
         cout<<"Element don't exist";
 
@@ -29,3 +30,12 @@ int main(){
 
 //output:
 
+// PS C:\Users\acer\CEC-Practice_problems\Sorting\QuickSort> g++ .\MajorityEle.cpp
+// PS C:\Users\acer\CEC-Practice_problems\Sorting\QuickSort> ./a.exe
+// 6
+// 4 2 2 2 2 1
+// majority element: 2
+// PS C:\Users\acer\CEC-Practice_problems\Sorting\QuickSort> ./a.exe
+// 4
+// 4 3 2 1
+// Element dont exist
